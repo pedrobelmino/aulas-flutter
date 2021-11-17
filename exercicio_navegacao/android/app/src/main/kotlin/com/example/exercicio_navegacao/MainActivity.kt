c2017693d00f0d03cade0b4cfd23f48decba99aa
pedrobelmino <pedrobelmino.com>
2021-11-17 19:04:03 -0300

    adicinando exercicio de navegação

---
+++ exercicio_navegacao/android/app/src/main/kotlin/com/example/exercicio_navegacao/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.exercicio_navegacao
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ exercicio_navegacao/lib/firstpage.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  void _goToSecondPage(BuildContext context) {
+    Navigator.pushNamed(context, "second_page");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                _goToSecondPage(context);
+              },
+              child: const Text("Go to second page")),
+        ],
+      ),
+    ));
+  }
+}
